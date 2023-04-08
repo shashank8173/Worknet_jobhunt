e86c28d8dfa8fffe1842e65ed745848c818944f0
Shashank singh chauhan <89065656+shashank8173@users.noreply.github.com>
2023-04-08 18:59:23 +0530

    Update form-handler.php
    Signed-off-by: Shashank singh chauhan <89065656+shashank8173@users.noreply.github.com>

--- form-handler.php
+++ form-handler.php
@@ -3,7 +3,6 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-
 $email_from = 'jobhuntfreshers100@gmail.com';
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
@@ -13,11 +12,5 @@ $email_body = "User Name: $name.\n".
 $to = 'samratsingh8173@gmail.com';
 $headers = "From: $email_from\r\n";
 $headers = "Reply-To: $visitor_email \r\n";
-
 mail($to,$email_subject,$email_body,$headers);
-
-header("Location: contact.html");
-
-
-
-?>
+header("Location: contact.html");?>
